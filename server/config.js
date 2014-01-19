196bacd1607e4eb229406a109e468e88c2d8bddf
Damien Saillard <dam.saillard@gmail.com>
2014-01-19 16:23:03 +0100

    add package.json for root project dir and listen to port 3000. ref #3

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ path = require('path');
 
 module.exports = {
   server: {
-    listenPort: 80,                                     // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
+    listenPort: 3000,                                     // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
     securePort: 8433,                                   // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8433 for instance)
     distFolder: path.resolve(__dirname, '../client/dist'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
     staticUrl: '/static',                               // The base url from which we serve static files (such as js, css and images)
