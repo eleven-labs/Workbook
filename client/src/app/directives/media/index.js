a9c4601f61ed3fc0bb1805e9f5fab343840382e0
Damien SAILLARD <dsaillard.groupeeleven@tf1.fr>
2014-03-27 10:33:49 +0100

    fix media directive when minified

--- client/src/app/directives/media/index.js
+++ client/src/app/directives/media/index.js
@@ -1,5 +1,65 @@
 angular.module('directives.media', ['security.authorization', 'ngSanitize'])
 
+.controller('MediaController', ['$scope', 'security', function($scope, security){
+  var updateSuccess = function(result) {
+    $scope.media = result;
+  };
+
+  var failsRequest = function() {
+    console.log('error');
+  };
+
+  $scope.focusMediaCommentArea = function($event) {
+    $($event.currentTarget).closest('.media-body').find('.form-control').focus();
+  };
+
+  $scope.userInArray = function(userIds) {
+    return userIds.indexOf(security.currentUser._id) !== -1;
+  };
+
+  $scope.like = function() {
+    $scope.media.$addLike(updateSuccess, failsRequest);
+  };
+
+  $scope.unlike = function() {
+    $scope.media.$removeLike(updateSuccess, failsRequest);
+  };
+
+  $scope.ownMedia = function() {
+    return $scope.media.creator === security.currentUser._id;
+  };
+
+  $scope.comment = function() {
+    if ($scope.message) {
+      $scope.media.$addComment(
+        $scope.message,
+        function success(result) {
+          $scope.message = '';
+          updateSuccess(result);
+        },
+        failsRequest
+      );
+    }
+  };
+
+  $scope.removeComment = function(comment) {
+    $scope.media.$removeComment(comment._id, updateSuccess, failsRequest);
+  };
+
+  $scope.displayPreviousComments = function() {
+    $scope.media.$getPreviousComments(
+      $scope.media.comments.length,
+      10,
+      function successRequest(lastComments) {
+        for (var i = Object.keys(lastComments).length - 1; i >= 0; i--) {
+          $scope.media.comments.unshift(lastComments[i]);
+        }
+      },
+      failsRequest
+    );
+  };
+}])
+
 .directive('media', function() {
   return {
     restrict: 'E',
@@ -10,67 +70,47 @@ angular.module('directives.media', ['security.authorization', 'ngSanitize'])
       'removeMedia': '&onMediaRemove'
     },
     templateUrl: 'directives/media/templates/media.tpl.html',
-    controller: function($scope, security) {
-      var updateSuccess = function(result) {
-        $scope.media = result;
-      };
-
-      var failsRequest = function() {
-        console.log('error');
-      };
-
-      $scope.focusMediaCommentArea = function($event) {
-        $($event.currentTarget).closest('.media-body').find('.form-control').focus();
-      };
-
-      $scope.userInArray = function(userIds) {
-        return userIds.indexOf(security.currentUser._id) !== -1;
-      };
-
-      $scope.like = function() {
-        $scope.media.$addLike(updateSuccess, failsRequest);
-      };
-
-      $scope.unlike = function() {
-        $scope.media.$removeLike(updateSuccess, failsRequest);
-      };
-
-      $scope.ownMedia = function() {
-        return $scope.media.creator === security.currentUser._id;
-      };
-
-      $scope.comment = function() {
-        if ($scope.message) {
-          $scope.media.$addComment(
-            $scope.message,
-            function success(result) {
-              $scope.message = '';
-              updateSuccess(result);
-            },
-            failsRequest
-          );
-        }
-      };
-
-      $scope.removeComment = function(comment) {
-        $scope.media.$removeComment(comment._id, updateSuccess, failsRequest);
-      };
-
-      $scope.displayPreviousComments = function() {
-        $scope.media.$getPreviousComments(
-          $scope.media.comments.length,
-          10,
-          function successRequest(lastComments) {
-            for (var i = Object.keys(lastComments).length - 1; i >= 0; i--) {
-              $scope.media.comments.unshift(lastComments[i]);
-            }
-          },
-          failsRequest
-        );
-      };
-    }
+    controller: 'MediaController'
   };
 })
+
+.controller('CommentController', ['$scope', 'security', function($scope, security){
+  var updateSuccess = function(commentId) {
+    return function() {
+      $scope.media.$getComment(
+        commentId,
+        function success(comment) {
+          $scope.comment = comment;
+        },
+        function error(result) {
+          console.log(result);
+        }
+      );
+    };
+  };
+
+  var updateError = function() {
+    console.log('error');
+  };
+
+  $scope.userInArray = function(userIds) {
+    if (userIds) {
+      return userIds.indexOf(security.currentUser._id) !== -1;
+    }
+  };
+
+  $scope.likeComment = function() {
+    $scope.media.$addLikeToComment($scope.comment._id, updateSuccess($scope.comment._id), updateError);
+  };
+
+  $scope.unlikeComment = function() {
+    $scope.media.$removeLikeFromComment($scope.comment._id, updateSuccess($scope.comment._id), updateError);
+  };
+
+  $scope.ownComment = function() {
+    return $scope.comment.creator === security.currentUser._id;
+  };
+}])
 
 .directive('comment', function() {
   return {
@@ -81,42 +121,6 @@ angular.module('directives.media', ['security.authorization', 'ngSanitize'])
       'removeComment': '&onCommentRemove'
     },
     templateUrl: 'directives/media/templates/comment.tpl.html',
-    controller: function($scope, security) {
-      var updateSuccess = function(commentId) {
-        return function() {
-          $scope.media.$getComment(
-            commentId,
-            function success(comment) {
-              $scope.comment = comment;
-            },
-            function error(result) {
-              console.log(result);
-            }
-          );
-        };
-      };
-
-      var updateError = function() {
-        console.log('error');
-      };
-
-      $scope.userInArray = function(userIds) {
-        if (userIds) {
-          return userIds.indexOf(security.currentUser._id) !== -1;
-        }
-      };
-
-      $scope.likeComment = function() {
-        $scope.media.$addLikeToComment($scope.comment._id, updateSuccess($scope.comment._id), updateError);
-      };
-
-      $scope.unlikeComment = function() {
-        $scope.media.$removeLikeFromComment($scope.comment._id, updateSuccess($scope.comment._id), updateError);
-      };
-
-      $scope.ownComment = function() {
-        return $scope.comment.creator === security.currentUser._id;
-      };
-    }
+    controller: 'CommentController'
   };
 });
